6d85f6b8049f400218bfcbd93db0925964d47245
Riya <ri.kulkarni02@gmail.com>
2026-03-01 06:26:44 -0800

    removed uneccesary code

--- llm-extraction.py
+++ llm-extraction.py
@@ -1,19 +1,6 @@
 import spacy
 from spacy.lang.en.examples import sentences 
 from transformers import pipeline
-nlp = spacy.load("en_core_web_sm")
-test_sentence = '''Watson & Crick section from a bio textbook. Rosalind Franklin materializes. Nettie Stevens (sex chromosomes) is another nearby node. This one basically demos itself.'''
-doc = nlp(test_sentence)
-print(doc.text)
-words = []
-for token in doc:
-    print(token.text, token.pos_)
-    if token.pos_ == "PROPN":
-        words.append(token.text)
-    print('Important Nouns')
-for word in words:
-    print(word)
-
 def extract_nouns(text):
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(text)
