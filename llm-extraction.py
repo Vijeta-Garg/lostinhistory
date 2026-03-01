0b560c76696da1197b3282af9227127ee68798f7
Riya <ri.kulkarni02@gmail.com>
2026-03-01 06:23:18 -0800

    Extract proper nouns with spacy

--- llm-extraction.py
+++ llm-extraction.py
@@ -1,28 +1,28 @@
 import spacy
 from spacy.lang.en.examples import sentences 
-from transformers import BertConfig, BertModel
 from transformers import pipeline
-import torch 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp(sentences[1])
+test_sentence = '''Watson & Crick section from a bio textbook. Rosalind Franklin materializes. Nettie Stevens (sex chromosomes) is another nearby node. This one basically demos itself.'''
+doc = nlp(test_sentence)
 print(doc.text)
 words = []
 for token in doc:
-    if token.pos_ == "NOUN" or token.pos_ == "PROPN":
+    print(token.text, token.pos_)
+    if token.pos_ == "PROPN":
         words.append(token.text)
+    print('Important Nouns')
 for word in words:
     print(word)
-    
-# unmasker = pipeline('fill-mask', model='bert-base-uncased')
 
-# unmasker("Artificial Intelligence [MASK] take over the world.")
-unmasker = pipeline(
-    task = 'fill-mask',
-    model = 'google-bert/bert-base-uncased',
-    dtype = torch.float16,
-    device = 0
-)
-pipeline("Plants create [MASK] through a process known as photosynthesis.")
-# configuration = BertConfig()
-# model = BertModel(configuration)
-# configuration = model.config
+def extract_nouns(text):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    words = []
+    for token in doc:
+        if token.pos_ == "PROPN":
+            words.append(token.text)
+    return words
+            
+
+
+
